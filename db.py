71e7eaa9b10d5b359c4727f639d588fbc1f31bc5
marcocmmattos <75364222+marcocmmattos@users.noreply.github.com>
2021-05-13 23:14:27 +0100

    Nova URL mongoDB

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 
 from flask import Flask
 from flask_pymongo import pymongo
-from app import app
-CONNECTION_STRING = "mongodb+srv://admin:admPass@constructionappcluster.qfh8l.mongodb.net/constructionDB?retryWrites=true&w=majority"
+from app import app 
+CONNECTION_STRING = "mongodb+srv://user:userPass@constructionappcluster.qfh8l.mongodb.net/constructionDB?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('constructionDB')
 user_collection = pymongo.collection.Collection(db, 'user')
